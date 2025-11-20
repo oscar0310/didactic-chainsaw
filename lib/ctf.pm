14b2c41089843126793b5898d1cb6656a2dc4424
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2025-11-20 10:33:26 +0100

    fix #11 añadiendo tipo a ctf
    contiene el tipo de reto CTF

--- lib/ctf.pm
+++ lib/ctf.pm
@@ -4,5 +4,16 @@ use strictures 2;
 use namespace::clean;
 
 
+use Types::Standard qw(ENum Str); #Import Enum
+
+has type => (  #Challenge type
+    is => 'ro',
+    isa => Enum[
+        'criptografía',
+        'ingeniería inversa',
+        'web',
+        'pwn'
+    ],
+);
 
 
