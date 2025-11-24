bf6ff34ce98c402fac23d24351c5566deba92bcb
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2025-11-24 19:15:00 +0100

    #13 creado el atributo description de la entidad CVE
    contendrá la descripción del CVE, se establece que sea un NonEmptyStr porque no tiene sentido una descripción vacía, por esto se añade el tipo NonEmptyStr y es requerido porque un CVE sin una descripción de lo que hace no sirve.

--- lib/cve.pm
+++ lib/cve.pm
@@ -1,6 +1,16 @@
-package cve;
+package cve; 
 use Moo;
 use strictures 2;
 use namespace::clean;
+
+
+use Types::Common::String qw(NonEmptyStr);
+
+
+has description => ( #CVE description
+    is => 'ro',
+    isa => NonEmptyStr,  # is not a empty string
+    required => 1,  
+);
 
 1;
