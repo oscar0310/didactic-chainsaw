7fe6531fe6a513012e52919c426fef47ca740409
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2025-11-24 19:24:40 +0100

    #13 resolviendo los comentarios de gosema
     Se ha creado una nueva secuencia de commits siguiendo las recomendaciones de gosema justificando el código

--- lib/cve.pm
+++ lib/cve.pm
@@ -1,19 +1,19 @@
 package cve; 
 use Moo;
 use strictures 2;
-use namespace::clean;
+use namespace::clean; #follow good perl practices
 
 
-use Types::Common::String qw(NonEmptyStr);
-use Types::Standard qw(ArrayRef);
+use Types::Common::String qw(NonEmptyStr); #Type: NonEMptyStr
+use Types::Standard qw(ArrayRef); #Type ArrayRef
 
-has description => ( #CVE description
+has description => ( # CVE description
     is => 'ro',
     isa => NonEmptyStr,  # is not a empty string
     required => 1,  
 );
 
-has links => ( #CVE links
+has links => ( # CVE links
     is =>'ro',
     isa => ArrayRef[NonEmptyStr],
     required => 1,
