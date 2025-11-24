d1f3806680361a189a11330e63451fd4562dc84b
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2025-11-24 19:19:33 +0100

    #13 creado el atributo links de la entidad CVE
    contendrá la links  del CVE para explotar la vulnerabilidad,  se establece que sea un Array de NonEmptyStr porque es un conjunto de links los cuales no pueden ser vacios, por esto se añade el tipo ArrayRef  y es requerido porque un CVE sin los links para explotar la vuylnerabilidad no sirve.

--- lib/cve.pm
+++ lib/cve.pm
@@ -5,12 +5,18 @@ use namespace::clean;
 
 
 use Types::Common::String qw(NonEmptyStr);
-
+use Types::Standard qw(ArrayRef);
 
 has description => ( #CVE description
     is => 'ro',
     isa => NonEmptyStr,  # is not a empty string
     required => 1,  
+);
+
+has links => ( #CVE links
+    is =>'ro',
+    isa => ArrayRef[NonEmptyStr],
+    required => 1,
 );
 
 1;
